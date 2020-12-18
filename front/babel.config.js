6b8acd8779517f5805ead5d0f5e334c6d13582db
subuta <shunta.3110@gmail.com>
2020-12-18 16:32:02 +0900

    Fix babel.

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'usage',
-        corejs: 3,
-      },
-    ],
-    '@babel/preset-react',
-  ],
+  presets: ['poi/babel'],
   plugins: ['relay'],
 }
